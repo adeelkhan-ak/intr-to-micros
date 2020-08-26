c7fa251484f7539592a030c05421f1557ac5f1a9
ak140 <ak140175@gmail.com>
2020-08-27 01:18:17 +0500

    Request Handler

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		log.Println("hello world")
+	})
 	http.ListenAndServe("127.0.0.1:9090", nil)
 
 }
